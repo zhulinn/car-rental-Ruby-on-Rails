adc19d37b1e9309eac530b5608c436bce2ad0373
Xinyu <xgong6@ncsu.edu>
2017-09-26 00:25:23 -0400

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.logger = Logger.new(STDOUT)
+  config.log_level = :info
 end
